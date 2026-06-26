45bc1ac880fc0b543a38d7081b54d625ecc064e3
Oscar Guerrero <oscargabrguerrero@gmail.com>
2026-06-26 16:59:58 -0400

    Add: house-robber - submission-4

---
+++ Data Structures & Algorithms/house-robber/submission-4.py
@@ -0,0 +1,13 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) <= 2:
+            return max(nums)
+        h = [0] * len(nums)
+        #basecase
+        h[0] = nums[0]
+        h[1] = nums[1]
+        for i in range(2, len(nums)):
+            h[i] = nums[i] + max(h[i-2], h[i-3])
+
+        return max(h[-1], h[-2])
+        
