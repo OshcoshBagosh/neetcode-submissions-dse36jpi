9a8e30346ef0c4e75e24c76fd0762f3e9bec2c88
Oscar Guerrero <oscargabrguerrero@gmail.com>
2026-07-16 19:21:40 -0400

    Add: word-break - submission-1

---
+++ Data Structures & Algorithms/word-break/submission-1.py
@@ -0,0 +1,22 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        memo = {}
+        def dfs(start):
+            if start in memo:
+                return memo[start]
+            if start == len(s):
+                return True
+            
+            for end in range(start+1, len(s)+1):
+                prefix = s[start:end]
+
+                if prefix in wordDict and dfs(end):
+                    memo[start] = True
+                    return True
+
+            memo[start] = False
+            return False
+    
+        return dfs(0)
+
+        
